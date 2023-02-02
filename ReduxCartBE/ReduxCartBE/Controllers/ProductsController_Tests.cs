8dbadc5f5ca56e614b4823a67933178ab4c350cb
Bryant Wooters <Jesusrox45@gmail.com>
2023-02-01 23:22:17 -0500

    Add controller unit tests

--- ReduxCartBE/ReduxCartBE/Controllers/ProductsController_Tests.cs
+++ ReduxCartBE/ReduxCartBE/Controllers/ProductsController_Tests.cs
@@ -1,23 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ReduxCartBE.Persistence.Queries;
 using Xunit;
+using Moq;
 
 namespace ReduxCartBE.Controllers;
 
 public class ProductsController_Tests
 {
     private readonly ProductsController _controller;
-    private readonly IProductQuery _query;
-    public ProductsController_Tests(ProductsController controller, ProductQuery query)
+    private readonly Mock<IProductQuery> _query;
+    public ProductsController_Tests()
     {
-        _controller = controller;
-        _query = query;
+        _query = new Mock<IProductQuery>();
+        _controller = new ProductsController(_query.Object);
     }
 
     [Fact]
     public void OnSuccessfulGetProductsList_ReturnsOkObjectResult()
     {
-        
+        var result = (OkObjectResult)_controller.GetProducts();
+        Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+    }
 
+    [Fact]
+    public void OnUnsuccessfulGetProductsList_ReturnsBadObjectResult()
+    {
+        _query.Setup(x => x.GetProducts())
+            .Throws(new Exception("Yea, you can't do that 'round these parts..."));
+
+        var result = (BadRequestObjectResult)_controller.GetProducts();
+        
+        Assert.Equal(StatusCodes.Status400BadRequest, result.StatusCode);
+    }
+
+    [Fact]
+    public void GetProductsList_CallsQuery()
+    {
+        _controller.GetProducts();
+
+        _query.Verify(x => x.GetProducts(), Times.Once);
     }
 }
